cd77cee37eb6a810ee1a7635835d2725542ee757
Tyler Chapman <TEEBQNE@gmail.com>
2019-05-23 21:51:34 -0400

    Added navmesh AI with simple enemy.

---
+++ Rebirth/Assets/BasicEnemyAIFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemyAIFollow : MonoBehaviour
+{
+    // variables for following player
+    Transform goal;          // transform of the player
+    NavMeshAgent agent;      // navmesh agent component of enemy
+
+    void Start()
+    {
+        // assign relevant data
+        goal = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // set the destination of the navmesh agent
+        agent.SetDestination(goal.position);
+    }
+}
