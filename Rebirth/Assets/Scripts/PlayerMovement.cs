c5bb2a7525541b90131fb9ae4f4b4c10438ba9b2
Mark <markbotaish@gmail.com>
2019-06-01 14:46:49 -0400

    Mark
    - Added AI
    - CriticalHitPoint

--- Rebirth/Assets/Scripts/PlayerMovement.cs
+++ Rebirth/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public int id;
     public float inputSensitivity = 150.0f;
 
+    public float alertEnemiesRadius = 10.0f;
+
     private Player player;
     [HideInInspector] public Rigidbody rb;
     [HideInInspector] public float gravMulti = 1.0f;
@@ -17,9 +19,12 @@ public class PlayerMovement : MonoBehaviour
 
     float height; //Half the height of the mesh 
 
+    LayerMask enemyLayer;
+
     // Start is called before the first frame update
-    virtual public void Start()
+    public void Start()
     {
+        enemyLayer  = LayerMask.GetMask("Waiting");
         rb = this.gameObject.GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
         height = (this.gameObject.GetComponent<MeshRenderer>().bounds.size.y / 2.0f) + 0.05f;
@@ -30,6 +35,19 @@ public class PlayerMovement : MonoBehaviour
     virtual public void Update()
     {
         checkMovement();
+        checkEnemies();
+    }
+
+    void checkEnemies()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, alertEnemiesRadius, enemyLayer);
+
+        foreach(Collider col in hitColliders)
+        {
+            print("Hit");
+            col.gameObject.AddComponent<BasicEnemyAIFollow>();
+            col.gameObject.layer = 8;
+        }
     }
 
     void checkMovement()
@@ -58,5 +76,13 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity += Vector3.up * Physics.gravity.y * 1.5f * Time.deltaTime * gravMulti;
         }
 
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "CriticalHitPoint")
+        {
+            print("Big Sad");
+        }
     }
 }
