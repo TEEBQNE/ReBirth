5b210aa7a2df87746ef43afd9dafdf4e0c1a8adf
Mark <markbotaish@gmail.com>
2019-06-09 20:57:16 -0400

    3D Menu

---
+++ Rebirth/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public enum ButtonType
+    {
+        PLAY, 
+        HELP, 
+        QUIT, 
+        BACK
+    };
+
+    public ButtonType Button;
+
+    Color color;
+    MeshRenderer meshRender;
+    MenuScript menu;
+
+    public void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+        color = meshRender.material.color;
+        menu = MenuScript.instance;
+    }
+
+    private void OnMouseOver()
+    {
+
+        if (!this.enabled)
+            return;
+
+        meshRender.material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!this.enabled)
+            return;
+
+        meshRender.material.color = color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!this.enabled)
+            return;
+
+        switch (Button)
+        {
+            case ButtonType.PLAY:
+                menu.playGame();
+                break;
+            case ButtonType.HELP:
+                menu.Help();
+                break;
+            case ButtonType.QUIT:
+                menu.QuitGame();
+                break;
+            case ButtonType.BACK:
+                menu.Back();
+                break;
+        }
+    }
+}
