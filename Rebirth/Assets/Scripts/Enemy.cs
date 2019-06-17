f4e41fb9ca4f1a570f1fe1f500bba3e92189f938
Forest <forest.moses@drake.edu>
2019-06-17 15:47:56 -0500

    Actually kinda working
    Got unity and git to stop being an ass, raycast needs tweaking. need to add firerate, reload, mods etc

---
+++ Rebirth/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float health = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
